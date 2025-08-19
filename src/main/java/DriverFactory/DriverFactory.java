40efe96efbc81a100f56679ff5fc5c82324465af
MohamedFaran <mohamed.khaled11793@gmail.com>
2025-08-19 13:51:23 +0300

    test1

--- src/main/java/DriverFactory/DriverFactory.java
+++ src/main/java/DriverFactory/DriverFactory.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
     private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
@@ -47,6 +48,7 @@ public class DriverFactory {
 
     }
 
+
     public static WebDriver getDriver() {
         return driverThreadLocal.get();
     }
@@ -54,5 +56,26 @@ public class DriverFactory {
     public static void quitDriver() {
         getDriver().quit();
         driverThreadLocal.remove();
+    }
+
+    public static WebDriver createDriverHeadless(String driverType) {
+        return switch (driverType.toLowerCase()) {
+            case "chrome" -> {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new");
+                yield new ChromeDriver(chromeOptions);
+            }
+            case "firefox" -> {
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                yield new FirefoxDriver(firefoxOptions);
+            }
+            case "edge" -> {
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--headless=new");
+                yield new EdgeDriver(edgeOptions);
+            }
+            default -> throw new IllegalArgumentException("Invalid driver type: " + driverType);
+        };
     }
 }
